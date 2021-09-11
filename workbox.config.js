18a34b3143a887d17f288fee1e2bf482640bd229
Alexander Shershnev <shershnev.av@gmail.com>
2021-09-11 16:27:53 +0300

    improve build

--- workbox.config.js
+++ workbox.config.js
@@ -2,7 +2,8 @@ module.exports = {
   swSrc: './build/sw.js',
   swDest: './build/sw.js',
   globDirectory: './build',
+  globPatterns: ['*.js', '*.css', '*.html', '*.webmanifest'],
   globIgnores: ['sw.*', '*.map', '*.cache'],
-  dontCacheBustURLsMatching: /\.build\.[0-9a-z]{8}\./,
+  dontCacheBustURLsMatching: /\.[0-9a-z]{8}\./,
   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024
 }
