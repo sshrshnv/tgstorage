d3d676c754f238c538a1cebd9701c7d95a3aeb6b
Alexander Shershnev <shershnev.av@gmail.com>
2022-01-27 17:53:31 +0300

    fix router

--- src/core/actions/actions.app-route.ts
+++ src/core/actions/actions.app-route.ts
@@ -1,9 +1,23 @@
 import { store } from '~/core/store'
 
+let listenerEnabled = false
+
 export const setAppRoute = (route: string) => {
-  history.pushState(history.state, document.title, route)
+  self.history.pushState(history.state, document.title, route)
 
   store.setState({
     appRoute: route
   })
+
+  if (!listenerEnabled) {
+    listenerEnabled = true
+
+    const handlePopState = () => {
+      store.setState({
+        appRoute: self.location.pathname
+      })
+    }
+
+    self.addEventListener('popstate', handlePopState)
+  }
 }
