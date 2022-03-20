f6bbc6c7ab868924819157368aea4a2749ec2016
Jia-Bin <cracky.ice@gmail.com>
2022-03-21 01:05:50 +0800

    Update detect-locale.ts

--- src/tools/detect-locale.ts
+++ src/tools/detect-locale.ts
@@ -1,12 +1,17 @@
+export const AVALIABLE_LOCALES = ['en', 'es', 'ru', 'zhTW'] as const
+export const EXACT_LOCALES = ['zh']
 export const CIS = ['ru', 'ua', 'by', 'ge', 'az', 'kz', 'uz', 'tj', 'kg']
-
-export const AVALIABLE_LOCALES = ['en', 'es', 'ru','zhTW'] as const
 export const FALLBACK_LOCALE= 'en' as const
 
 export type AvailableLocales = typeof AVALIABLE_LOCALES[number]
 
 export const detectLocale = (): AvailableLocales => {
-  const locale = self.navigator.language.slice(0,2) as AvailableLocales
+  let locale = self.navigator.language.slice(0, 2) as AvailableLocales
+
+  if (EXACT_LOCALES.includes(locale)) {
+    locale = self.navigator.language.replace('-', '') as AvailableLocales
+  }
+
   return (
     AVALIABLE_LOCALES.includes(locale) ? locale :
       CIS.includes(locale) ? 'ru' :
