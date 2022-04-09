3ecce746c8af1827abf1af0f8d10469cfa70eab3
Alexander Shershnev <shershnev.av@gmail.com>
2022-04-09 22:19:12 +0300

    Fix analytics proxy

--- src/scripts/analytics.tsx
+++ src/scripts/analytics.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponent as FC, RefObject } from 'preact'
 import { h, Fragment } from 'preact'
 import { memo, createPortal } from 'preact/compat'
+import { useState, useCallback } from 'preact/hooks'
 
 import type { User } from '~/core/store'
 import { checkIsStandalone } from '~/tools/detect-standalone'
@@ -15,29 +16,49 @@ const bodyEl = self.document.body
 export const Analytics: FC<Props> = memo(({
   userRef
 }) => {
+  const [proxied, setProxied] = useState(false)
+  const [loaded, setLoaded] = useState(false)
+
+  const handleLoad = useCallback(() => {
+    setLoaded(true)
+  }, [setLoaded])
+
+  const handleError = useCallback(() => {
+    setProxied(true)
+  }, [setProxied])
+
   return process.env.GOOGLE_ANALYTICS_ID ? createPortal((
     <Fragment>
       <script
-        src={`/proxy/tag/${process.env.GOOGLE_ANALYTICS_ID}`}
+        src={proxied ?
+          `/proxy/tag/${process.env.GOOGLE_ANALYTICS_ID}` :
+          `https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`
+        }
         async
+        onLoad={handleLoad}
+        onError={handleError}
       ></script>
-      <script dangerouslySetInnerHTML={{ __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
-          transport_url: 'https://${self.location.hostname}/proxy/event',
-          anonymize_ip: true,
-          country: '${userRef.current?.country || 'unknown'}',
-          lang: '${self.navigator.language}',
-          device: '${self.navigator.userAgent}',
-          os: '${getOS()}',
-          browser: '${getBrowser()}',
-          authenticated: ${!!userRef.current},
-          installed: ${checkIsStandalone()},
-        });
-      `}}>
-      </script>
+      {(loaded || proxied) && (
+        <script
+          dangerouslySetInnerHTML={{ __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+              ${proxied ? `transport_url: 'https://${self.location.hostname}/proxy/event',` : ''}
+              anonymize_ip: true,
+              country: '${userRef.current?.country || 'unknown'}',
+              lang: '${self.navigator.language}',
+              device: '${self.navigator.userAgent}',
+              os: '${getOS()}',
+              browser: '${getBrowser()}',
+              authenticated: ${!!userRef.current},
+              installed: ${checkIsStandalone()},
+              proxied: ${proxied}
+            });
+          `}}
+        ></script>
+      )}
     </Fragment>
   ), bodyEl) : null
 })
